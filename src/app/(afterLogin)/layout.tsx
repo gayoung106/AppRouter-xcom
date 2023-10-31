ef6905de22c3700918e1ae881ce561788a87e6b3
SongGayoung <xjongamx@kyonggi.ac.kr>
2023-10-31 17:02:40 +0900

    레이아웃

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import style from "@/app/(afterLogin)/layout.module.css";
-
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../../public/favicon.svg";
 export default function AfterLoginLayout({
   children,
 }: {
@@ -9,12 +11,24 @@ export default function AfterLoginLayout({
   return (
     <div className={style.container}>
       <header className={style.leftSectionWrapper}>
-        <section className={style.leftSection}></section>
+        <section className={style.leftSection}>
+          <div className={style.leftSectionFixed}>
+            <Link className={style.logo} href="/home">
+              <div className={style.logoPill}>
+                <Image src={Logo} alt="로고" width={40} height={40} />
+              </div>
+            </Link>
+          </div>
+        </section>
       </header>
       <div className={style.rightSectionWrapper}>
         <div className={style.rightSectionInner}>
           <main className={style.main}>{children}</main>
-          <section className={style.rightSection}></section>
+          <section className={style.rightSection}>
+            <form className={style.search}>
+              <input type="search" />
+            </form>
+          </section>
         </div>
       </div>
     </div>
